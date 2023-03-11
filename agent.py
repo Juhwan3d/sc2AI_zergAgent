45953d9c76c123daefa21864ce9f32521af43adf
Juhwan Yoon <yoon987654@gmail.com>
2023-03-11 20:19:03 +0900

    Update agent.py

---
+++ agent.py
@@ -0,0 +1,8 @@
+import random
+import numpy as np
+import pandas as pd
+import os
+from absl import app
+from pysc2.agents import base_agent
+from pysc2.lib import actions, features, units
+from pysc2.env import sc2_env, run_loop
